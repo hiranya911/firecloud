9ebd2b92ae64c81bb112fb827467fc971d81f99e
hiranya911 <hiranya911@gmail.com>
2019-01-26 14:32:28 -0800

    Moview review scorer function

---
+++ review-scorer/scorer/go.mod
@@ -0,0 +1,7 @@
+module github.com/hiranya911/firecloud/review-scorer/scorer
+
+require (
+	cloud.google.com/go v0.35.1 // indirect
+	firebase.google.com/go v3.6.0+incompatible
+	golang.org/x/oauth2 v0.0.0-20190115181402-5dab4167f31c // indirect
+)
---
+++ review-scorer/scorer/scorer.go
@@ -0,0 +1,77 @@
+package scorer
+
+import (
+	"context"
+	"log"
+	"time"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
+)
+
+// FirestoreEvent is the payload of a Firestore event.
+type FirestoreEvent struct {
+	OldValue   FirestoreValue `json:"oldValue"`
+	Value      FirestoreValue `json:"value"`
+	UpdateMask struct {
+		FieldPaths []string `json:"fieldPaths"`
+	} `json:"updateMask"`
+}
+
+// FirestoreValue holds Firestore fields.
+type FirestoreValue struct {
+	CreateTime time.Time `json:"createTime"`
+	// Fields is the data for this value. The type depends on the format of your
+	// database. Log an interface{} value and inspect the result to see a JSON
+	// representation of your database fields.
+	Fields     Review    `json:"fields"`
+	Name       string    `json:"name"`
+	UpdateTime time.Time `json:"updateTime"`
+}
+
+// Review represents the Firestore schema of a movie review.
+type Review struct {
+	Author string `json:"author"`
+	Text   string `json:"text"`
+}
+
+// client is a Firestore client, reused between function invocations.
+var client *db.Client
+
+func init() {
+	ctx := context.Background()
+
+	conf := &firebase.Config{
+		DatabaseURL: "https://solarflares-f4bee.firebaseio.com/",
+	}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalf("firebase.NewApp: %v", err)
+	}
+
+	client, err = app.Database(ctx)
+	if err != nil {
+		log.Fatalf("app.Firestore: %v", err)
+	}
+}
+
+// ScoreReview generates the scores for movie reviews and transactionally writes them to the
+// Firebase Realtime Database.
+func ScoreReview(ctx context.Context, e FirestoreEvent) error {
+	review := e.Value.Fields
+	reviweScore := score(review.Text)
+
+	ref := client.NewRef("scores").Child(review.Author)
+	updateTxn := func(node db.TransactionNode) (interface{}, error) {
+		var currentScore int
+		if err := node.Unmarshal(&currentScore); err != nil {
+			return nil, err
+		}
+		return currentScore + reviweScore, nil
+	}
+	return ref.Transaction(ctx, updateTxn)
+}
+
+func score(text string) int {
+	return len(text)
+}
