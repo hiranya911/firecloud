30f952595418db3b3d25de67e36dddb60c60e142
Hiranya Jayathilaka <hiranya911@gmail.com>
2020-01-29 22:01:28 -0800

    [chore] Release 0.0.2 retry take 2 (#12)

--- .github/scripts/check_tag.sh
+++ .github/scripts/check_tag.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Release version is ${RELEASE_VERSION}"
-echo "Reuse tag is set to ${RETRY_RELEASE}"
+echo "Retry release is set to ${RETRY_RELEASE}"
 
 git fetch --depth=1 origin +refs/tags/*:refs/tags/*
 
-RELEASE_TAG=`git describe --tags v${RELEASE_VERSION} 2> /dev/null`
+RELEASE_TAG=`git describe --tags ${RELEASE_VERSION} 2> /dev/null`
 if [[ $? -eq 0 ]];
 then
     echo "Tag ${RELEASE_VERSION} already exists."
-    if [[ -z "${RETRY_RELEASE}" ]];
+    if [[ "${RETRY_RELEASE}" != "true" ]];
     then
         echo "RETRY_RELEASE option is not set. Exiting."
         exit 1
@@ -18,6 +18,6 @@ then
         echo "::set-output name=scratch::false"
     fi
 else
-    echo "Tag v${RELEASE_VERSION} does not exist. It will be created."
+    echo "Tag ${RELEASE_VERSION} does not exist. It will be created."
     echo "::set-output name=scratch::true"
 fi
