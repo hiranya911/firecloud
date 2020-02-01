fec7be8b847587cab8d276dc09314d2489ade5af
hiranya911 <hiranya911@gmail.com>
2020-02-01 14:42:13 -0800

    Updated preflight script

--- .github/scripts/prepare_release.sh
+++ .github/scripts/prepare_release.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+############################# Input environment variables ##########################
+
+# 1. GITHUB_SHA: Git revision to release from (set by GitHub Actions).
+# 2. GITHUB_ACTOR: User that triggered the release (set by GitHub Actions).
+# 3. DRYRUN_RELEASE: "true" to run the workflow in the dryrun mode.
+# 4. SKIP_TWEET: "true" to suppress the Tweet at the end of workflow.
+
 ###################################### Outputs #####################################
 
 # 1. version: The version of this release including the 'v' prefix (e.g. v1.2.3).
 # 2. publish: Set when not executing in the dryrun mode.
-# 3. tweet: Set when the release should be posted to Twitter. Only set when
-#    publish=true.
+# 3. tweet: Set when the release should be posted to Twitter. Only set if
+#    publish is also set.
 # 4. changelog: Formatted changelog text for this release.
 
 ####################################################################################
@@ -13,77 +20,130 @@
 set -e
 set -u
 
-echo "Running release workflow at revision: ${GITHUB_SHA}"
-echo "Workflow triggered by: ${GITHUB_ACTOR}"
-echo "[release:dryrun]: ${DRYRUN_RELEASE}"
-echo "[release:skip-tweet]: ${SKIP_TWEET}"
-echo ""
+function echo_info() {
+    local MESSAGE=$1
+    echo "[INFO] ${MESSAGE}"
+}
 
-# Find release version.
-RELEASE_VERSION=`python -c "exec(open('release_demo/__about__.py').read()); print(__version__)"` || true
+function echo_warn() {
+    local MESSAGE=$1
+    echo "[WARN] ${MESSAGE}"
+}
+
+function terminate() {
+    echo ""
+    echo_warn "--------------------------------------------"
+    echo_warn "PREFLIGHT FAILED"
+    echo_warn "--------------------------------------------"
+    exit 1
+}
+
+
+echo_info "Starting release preflight..."
+echo_info "Git revision          : ${GITHUB_SHA}"
+echo_info "Workflow triggered by : ${GITHUB_ACTOR}"
+echo_info "Dry run mode          : ${DRYRUN_RELEASE}"
+echo_info "Skip Tweet            : ${SKIP_TWEET}"
+
+
+echo_info ""
+echo_info "--------------------------------------------"
+echo_info "Extracting release version"
+echo_info "--------------------------------------------"
+echo_info ""
+
+readonly ABOUT_FILE="release_demo/__about__.py"
+echo_info "Loading version from ${ABOUT_FILE}..."
+
+readonly VERSION_SCRIPT="exec(open('${ABOUT_FILE}').read()); print(__version__)"
+readonly RELEASE_VERSION=`python -c "${VERSION_SCRIPT}"` || true
 if [[ -z "${RELEASE_VERSION}" ]]; then
-  echo "Failed to extract release version from firebase_admin/__about__.py. Exiting."
-  exit 1
+  echo_warn "Failed to extract release version from: ${ABOUT_FILE}"
+  terminate
 fi
 
 if [[ ! "${RELEASE_VERSION}" =~ ^([0-9]*)\.([0-9]*)\.([0-9]*)$ ]]; then
-  echo "Malformed release version string: ${RELEASE_VERSION}. Exiting."
-  exit 1
+  echo_warn "Malformed release version string: ${RELEASE_VERSION}"
+  terminate
 fi
 
-echo "Extracted release version: ${RELEASE_VERSION}"
+echo_info "Extracted release version: ${RELEASE_VERSION}"
 echo "::set-output name=version::v${RELEASE_VERSION}"
 
-# Handle dryrun mode.
-if [[ "$DRYRUN_RELEASE" == "true" ]]; then
-  echo "Dryrun mode has been requested. No new tags or artifacts will be published."
-else
-  echo "Dryrun mode has NOT been requested."
-  echo "A new tag will be created, and release artifacts posted to Pypi."
+
+echo_info ""
+echo_info "--------------------------------------------"
+echo_info "Processing workflow options"
+echo_info "--------------------------------------------"
+echo_info ""
+
+if [[ "${DRYRUN_RELEASE}" != "true" ]]; then
+  echo_info "A new tag will be created, and release artifacts posted to Pypi."
   echo "::set-output name=publish::true"
 
   if [[ "${SKIP_TWEET}" != "true" ]]; then
-    echo "Release will be posted to Twitter upon successful completion."
+    echo_info "Release will be posted to Twitter upon successful completion."
     echo "::set-output name=tweet::true"
   else
-    echo "Skip Tweet mode has been requested. Release will not be posted to Twitter."
+    echo_info "Skip Tweet mode has been requested. Release will not be posted to Twitter."
   fi
+else
+  echo_info "Dry run mode has been requested. No new tags or artifacts will be published."
 fi
 
-# Fetch all tags.
-git fetch --depth=1 origin +refs/tags/*:refs/tags/*
 
-# Check if this release is already tagged.
-EXISTING_TAG=`git rev-parse -q --verify "refs/tags/v${RELEASE_VERSION}"` || true
+echo_info ""
+echo_info "--------------------------------------------"
+echo_info "Checking release tag"
+echo_info "--------------------------------------------"
+echo_info ""
+
+echo_info "---< git fetch --depth=1 origin +refs/tags/*:refs/tags/* >---"
+git fetch --depth=1 origin +refs/tags/*:refs/tags/*
+echo ""
+
+readonly EXISTING_TAG=`git rev-parse -q --verify "refs/tags/v${RELEASE_VERSION}"` || true
 if [[ -n "${EXISTING_TAG}" ]]; then
   if [[ "${DRYRUN_RELEASE}" != "true" ]]; then
-    RELEASE_URL="https://github.com/hiranya911/firecloud/releases/tag/v${RELEASE_VERSION}"
-    echo "Tag v${RELEASE_VERSION} already exists. Exiting."
-    echo "If the tag was created in a previous unsuccessful attempt, delete it and try again."
-    echo "Delete any corresponding releases at ${RELEASE_URL}."
-    echo "  $ git tag -d v${RELEASE_VERSION}"
-    echo "  $ git push --delete origin v${RELEASE_VERSION}"
-    exit 1
+    echo_warn "Tag v${RELEASE_VERSION} already exists."
+    echo_warn "If the tag was created in a previous failed attempt, delete it and try again."
+    echo_warn "   $ git tag -d v${RELEASE_VERSION}"
+    echo_warn "   $ git push --delete origin v${RELEASE_VERSION}"
+
+    readonly RELEASE_URL="https://github.com/hiranya911/firecloud/releases/tag/v${RELEASE_VERSION}"
+    echo_warn "Delete any corresponding releases at ${RELEASE_URL}"
+    terminate
   fi
-  echo "Tag v${RELEASE_VERSION} already exists. Ignoring in the dryrun mode."
+  echo_info "Tag v${RELEASE_VERSION} already exists. Ignoring in the dry run mode."
 else
-  echo "Tag v${RELEASE_VERSION} does not exist."
+  echo_info "Tag v${RELEASE_VERSION} does not exist."
 fi
 
-# Fetch history of the master branch.
-git fetch origin master --prune --unshallow
 
-# Generate changelog from commit history.
-echo "Generating changelog from history."
+echo_info ""
+echo_info "--------------------------------------------"
+echo_info "Generating changelog"
+echo_info "--------------------------------------------"
+echo_info ""
+
+echo_info "---< git fetch origin master --prune --unshallow >---"
+git fetch origin master --prune --unshallow
 echo ""
-CURRENT_DIR=$(dirname "$0")
-CHANGELOG=`${CURRENT_DIR}/generate_changelog.sh`
+echo_info "Generating changelog from history..."
+readonly CURRENT_DIR=$(dirname "$0")
+readonly CHANGELOG=`${CURRENT_DIR}/generate_changelog.sh`
 echo "$CHANGELOG"
 
 # Parse and preformat the text to handle multi-line output.
 # See https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/td-p/37870
-FILTERED_CHANGELOG=`echo "$CHANGELOG" | grep -v "\\[info\\]"`
+FILTERED_CHANGELOG=`echo "$CHANGELOG" | grep -v "\\[INFO\\]"`
 FILTERED_CHANGELOG="${FILTERED_CHANGELOG//'%'/'%25'}"
 FILTERED_CHANGELOG="${FILTERED_CHANGELOG//$'\n'/'%0A'}"
 FILTERED_CHANGELOG="${FILTERED_CHANGELOG//$'\r'/'%0D'}"
 echo "::set-output name=changelog::${FILTERED_CHANGELOG}"
+
+
+echo ""
+echo_info "--------------------------------------------"
+echo_info "PREFLIGHT SUCCESSFUL"
+echo_info "--------------------------------------------"
