064212201dc306b75aa4e122cc23ac77b5cc1bee
Hiranya Jayathilaka <hiranya911@gmail.com>
2020-02-11 12:08:41 -0800

    Added more logging to script (#33)

--- .github/scripts/merge_to_target.sh
+++ .github/scripts/merge_to_target.sh
@@ -7,10 +7,17 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
-git fetch origin dev --unshallow
-git fetch origin test-target --unshallow
+echo "Fetching dev..."
+git fetch origin dev
 
-git checkout master
+echo "Fetching test-target..."
+git fetch origin test-target
+
+echo "Switching to master..."
+git checkout test-target
+
+echo "Merging dev to test-target..."
 git merge dev
 
+echo "Pushing test-target..."
 git push origin test-target
