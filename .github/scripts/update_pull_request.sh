b26af45919229ec2902be51329e35c3ec2e76f8a
hiranya911 <hiranya911@gmail.com>
2020-02-11 21:34:12 -0800

    PR update script

---
+++ .github/scripts/update_pull_request.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -u
+
+readonly COMMENTS_URL="https://api.github.com/repos/hiranya911/firecloud/issues/${PR_NUMBER}/comments"
+readonly STATUS=" github-actions\\[bot\\] Staging successful$"
+readonly JQ_PATTERN=".[] | (.id|tostring) + \" \" + .user.login + \" \" + .body[0:50]"
+readonly STATUS_UPDATED=`curl -s ${COMMENTS_URL} | jq -r "${JQ_PATTERN}" | grep "${STATUS}"` || true
+if [[ -z "${STATUS_UPDATED}" ]]; then
+    if [[ "${STAGING_SUCCESSFUL}" == "true" ]]; then
+        echo "Adding comment"
+        curl "${COMMENTS_URL}" \
+            -s \
+            -d '{"body": "Staging successful"}' \
+            -o /dev/null \
+            -H "Authorization: Bearer ${GITHUB_TOKEN}"
+    fi
+else
+    if [[ "${STAGING_SUCCESSFUL}" == "false" ]]; then
+        readonly COMMENT_ID=`echo ${STATUS_UPDATED} | awk '{print $1}'`
+        echo "Deleting comment ${COMMENT_ID}"
+        curl "https://api.github.com/repos/hiranya911/firecloud/issues/comments/${COMMENT_ID}" \
+            -s \
+            -X DELETE \
+            -o /dev/null \
+            -H "Authorization: Bearer ${GITHUB_TOKEN}"
+    fi
+fi
