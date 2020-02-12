30e84adebdd36b957e0ad11fa6a1a7997194de39
Hiranya Jayathilaka <hiranya911@gmail.com>
2020-02-11 21:57:32 -0800

    [chore] Release 0.0.13 (#36)
    * [chore] Release 0.0.13
    
    * Adding auto approval
    
    * Using COMMENT instead of APPROVE
    
    * Logging review
    
    * PR update script
    
    * Updated how status is determined
    
    * Forced failure simulation
    
    * Parsing PR number from ref

---
+++ .github/scripts/update_pull_request.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -u
+
+readonly PR_NUMBER=`echo ${GITHUB_REF} | awk -F/ '{print $3}'`
+readonly COMMENTS_URL="https://api.github.com/repos/hiranya911/firecloud/issues/${PR_NUMBER}/comments"
+readonly STATUS=" github-actions\\[bot\\] Staging successful$"
+readonly JQ_PATTERN=".[] | (.id|tostring) + \" \" + .user.login + \" \" + .body[0:50]"
+readonly STATUS_UPDATED=`curl -s ${COMMENTS_URL} | jq -r "${JQ_PATTERN}" | grep "${STATUS}"` || true
+if [[ -z "${STATUS_UPDATED}" ]]; then
+    if [[ -n "${STAGING_SUCCESSFUL}" ]]; then
+        echo "Adding comment"
+        curl "${COMMENTS_URL}" \
+            -s \
+            -d '{"body": "Staging successful"}' \
+            -o /dev/null \
+            -H "Authorization: Bearer ${GITHUB_TOKEN}"
+    fi
+else
+    if [[ -z "${STAGING_SUCCESSFUL}" ]]; then
+        readonly COMMENT_ID=`echo ${STATUS_UPDATED} | awk '{print $1}'`
+        echo "Deleting comment ${COMMENT_ID}"
+        curl "https://api.github.com/repos/hiranya911/firecloud/issues/comments/${COMMENT_ID}" \
+            -s \
+            -X DELETE \
+            -o /dev/null \
+            -H "Authorization: Bearer ${GITHUB_TOKEN}"
+    fi
+fi
